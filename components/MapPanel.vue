1ce41553de82948184ec2d0490ae3c450d2740a7
yji35j42012 <yji35j42012@gmail.com>
2024-06-26 14:27:48 +0800

    文件紀錄

--- components/MapPanel.vue
+++ components/MapPanel.vue
@@ -1,25 +1,38 @@
 <template>
 	<div class="mappanel">
-		<div class="mappanel_box">
-			<div class="side">
-				<div class="side_box _top"></div>
-				<div class="side_box _left"></div>
-				<div class="side_box _right"></div>
+		<div id="mappanel_box" class="mappanel_box">
+			<div class="mappanel_box_row">
+				<div v-for="(index) in 5" :key="index" class="side"
+					:style="'transform: rotateX(-30deg) rotateY(290deg) translateX(0px) translateY(' + index * 0 + 'px);left:' + (index) * 50 + 'px;top:' + index * -20 + 'px;'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
 			</div>
-			<div class="rode">
-				<div class="ball"></div>
+			<div class="mappanel_box_row" style="top: 100px;left: 50px">
+				<div v-for="(index) in 5" :key="index" class="side"
+					:style="'transform: rotateX(-30deg) rotateY(290deg) translateX(0px) translateY(' + index * 0 + 'px);left:' + (index) * 50 + 'px;top:' + index * -20 + 'px;'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
 			</div>
-			<div class="side">
-				<div class="side_box _top"></div>
-				<div class="side_box _left"></div>
-				<div class="side_box _right"></div>
+
+
+			<div class="mappanel_box_row" style="top: 200px;left: 100px">
+				<div v-for="(index) in 5" :key="index" class="side"
+					:style="'transform: rotateX(-30deg) rotateY(290deg) translateX(0px) translateY(' + index * 0 + 'px);left:' + (index) * 50 + 'px;top:' + index * -20 + 'px;'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
 			</div>
-			<div class="side">
-				<div class="side_box _top"></div>
-				<div class="side_box _left"></div>
-				<div class="side_box _right"></div>
-			</div>
+			<!-- <div class="rode"></div>
+			<div class="rode"></div> -->
 		</div>
+
+
+		<canvas id="mycanvas" class="mappanel_box_canvas" width="1000" height="700"></canvas>
 	</div>
 </template>
 
@@ -27,7 +40,62 @@
 export default {
 	data() {
 		return {
+			pointData: [
+				{ x1: 900, y1: 100, x2: 80, y2: 75, text: '巡邏點1' },
+				{ x1: 80, y1: 75, x2: 200, y2: 300, text: '巡邏點2' },
+				{ x1: 200, y1: 300, x2: 580, y2: 500, text: '巡邏點3' },
+				{ x1: 580, y1: 500, x2: 900, y2: 100, text: '巡邏點4' },
+			]
 		}
 	},
+	created() {
+		this.doDraw();
+
+	},
+	mounted() {
+		let mappanel_box=document.getElementById('mappanel_box');
+		let canvas=document.getElementById('mycanvas');
+		canvas.style=`width:${ mappanel_box.offsetWidth }px; height:${ mappanel_box.offsetHeight }px;`
+		console.log('w', mappanel_box.offsetWidth);
+		console.log('w', mappanel_box.offsetHeight);
+	},
+	methods: {
+		doDraw() {
+			let canvas=document.getElementById('mycanvas');
+			let a=setInterval(() => {
+				canvas=document.getElementById('mycanvas');
+				if (!canvas) {
+					return false;
+				} else {
+					clearInterval(a);
+					let context=canvas.getContext('2d');
+					this.clearCanvas(context, canvas);
+					this.pointData.forEach((item) => {
+						const { x1, y1, x2, y2, text }=item
+
+						context.lineWidth=3;
+						context.lineCap='round';
+						context.strokeSytle='rgb(20,20,235)';
+						context.beginPath();
+
+						context.shadowColor='rgb(20,20,235)';
+						context.shadowOffsetX=0;
+						context.shadowOffsetY=0
+						context.shadowBlur=6;
+
+						context.setLineDash([5, 10]);
+
+						context.moveTo(x1, y1);
+						context.lineTo(x2, y2);
+
+						context.stroke();
+					})
+				}
+			})
+		},
+		clearCanvas(ctx, canvas) {
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+		}
+	}
 };
 </script>
