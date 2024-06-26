db929e22fb90ef54ec21ef2eee5187e24086fffd
yji35j42012 <yji35j42012@gmail.com>
2024-06-26 17:50:20 +0800

    檔案同步

--- components/MapPanel.vue
+++ components/MapPanel.vue
@@ -2,24 +2,25 @@
 	<div class="mappanel">
 		<div id="mappanel_box" class="mappanel_box">
 			<div class="mappanel_box_row">
-				<div v-for="(index) in 5" :key="index" class="side"
-					:style="'transform: rotateX(-30deg) rotateY(290deg) translateX(0px) translateY(' + index * 0 + 'px);left:' + (index) * 50 + 'px;top:' + index * -20 + 'px;'">
+				<div v-for="(index) in 10" :key="index" class="side"
+					:style="'transform: rotateX(-30deg) rotateY(290deg) translateX(0px) translateY(' + index * 0 + 'px);left:' + (index) * 0 + 'px;top:' + index * -20 + 'px;'">
 					<div class="side_box _top"></div>
 					<div class="side_box _left"></div>
 					<div class="side_box _right"></div>
 				</div>
 			</div>
-			<div class="mappanel_box_row" style="top: 100px;left: 50px">
-				<div v-for="(index) in 5" :key="index" class="side"
-					:style="'transform: rotateX(-30deg) rotateY(290deg) translateX(0px) translateY(' + index * 0 + 'px);left:' + (index) * 50 + 'px;top:' + index * -20 + 'px;'">
+			<!-- style="top: 100px;left: 50px" -->
+			<div class="mappanel_box_row" style="top: 0px; padding-left: 50px; display: none1;">
+				<div v-for="(index) in 10" :key="index" class="side"
+					:style="'transform: rotateX(-30deg) rotateY(290deg) translateX(0px) translateY(' + index * 0 + 'px);left:' + (index) * 0 + 'px;top:' + index * -20 + 'px;'">
 					<div class="side_box _top"></div>
 					<div class="side_box _left"></div>
 					<div class="side_box _right"></div>
 				</div>
 			</div>
 
-
-			<div class="mappanel_box_row" style="top: 200px;left: 100px">
+			<!-- style="top: 200px;left: 100px" -->
+			<div class="mappanel_box_row" style="top: 200px; padding-left: 100px; display: none;">
 				<div v-for="(index) in 5" :key="index" class="side"
 					:style="'transform: rotateX(-30deg) rotateY(290deg) translateX(0px) translateY(' + index * 0 + 'px);left:' + (index) * 50 + 'px;top:' + index * -20 + 'px;'">
 					<div class="side_box _top"></div>
@@ -50,14 +51,16 @@ export default {
 	},
 	created() {
 		this.doDraw();
-
 	},
 	mounted() {
-		let mappanel_box=document.getElementById('mappanel_box');
-		let canvas=document.getElementById('mycanvas');
-		canvas.style=`width:${ mappanel_box.offsetWidth }px; height:${ mappanel_box.offsetHeight }px;`
-		console.log('w', mappanel_box.offsetWidth);
-		console.log('w', mappanel_box.offsetHeight);
+		let cw=document.querySelectorAll('#mappanel_box .mappanel_box_row:nth-child(1) > .side');
+
+		console.log('cw',cw);
+		// let mappanel_box_h=document.getElementById('mappanel_box');
+		// let canvas=document.getElementById('mycanvas');
+		// canvas.style=`width:${ mappanel_box[0].offsetWidth }px; height:${ mappanel_box_h.offsetHeight }px;`
+		// console.log('w', mappanel_box[0].offsetWidth);
+		// console.log('w', mappanel_box[0].offsetHeight);
 	},
 	methods: {
 		doDraw() {
@@ -75,15 +78,16 @@ export default {
 
 						context.lineWidth=3;
 						context.lineCap='round';
-						context.strokeSytle='rgb(20,20,235)';
+						context.strokeSytle='#fff';
+						context.fillStyle='rgb(0,255,255)';
 						context.beginPath();
 
-						context.shadowColor='rgb(20,20,235)';
-						context.shadowOffsetX=0;
-						context.shadowOffsetY=0
-						context.shadowBlur=6;
+						// context.shadowColor='rgb(20,20,235)';
+						// context.shadowOffsetX=0;
+						// context.shadowOffsetY=0
+						// context.shadowBlur=6;
 
-						context.setLineDash([5, 10]);
+						// context.setLineDash([5, 10]);
 
 						context.moveTo(x1, y1);
 						context.lineTo(x2, y2);
