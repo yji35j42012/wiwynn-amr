678f7ed4472218ac61b378dfe7c494e08303b800
yji35j42012 <yji35j42012@gmail.com>
2024-06-28 18:21:13 +0800

    檔案同步

--- components/MapPanel.vue
+++ components/MapPanel.vue
@@ -1,39 +1,8 @@
 <template>
 	<div class="mappanel">
 		<div id="mappanel_box" class="mappanel_box">
-			<div class="mappanel_box_row">
-				<div v-for="(index) in 10" :key="index" class="side"
-					:style="'transform: rotateX(-30deg) rotateY(290deg) translateX(0px) translateY(' + index * 0 + 'px);left:' + (index) * 0 + 'px;top:' + index * -20 + 'px;'">
-					<div class="side_box _top"></div>
-					<div class="side_box _left"></div>
-					<div class="side_box _right"></div>
-				</div>
-			</div>
-			<!-- style="top: 100px;left: 50px" -->
-			<div class="mappanel_box_row" style="top: 0px; padding-left: 50px; display: none1;">
-				<div v-for="(index) in 10" :key="index" class="side"
-					:style="'transform: rotateX(-30deg) rotateY(290deg) translateX(0px) translateY(' + index * 0 + 'px);left:' + (index) * 0 + 'px;top:' + index * -20 + 'px;'">
-					<div class="side_box _top"></div>
-					<div class="side_box _left"></div>
-					<div class="side_box _right"></div>
-				</div>
-			</div>
-
-			<!-- style="top: 200px;left: 100px" -->
-			<div class="mappanel_box_row" style="top: 200px; padding-left: 100px; display: none;">
-				<div v-for="(index) in 5" :key="index" class="side"
-					:style="'transform: rotateX(-30deg) rotateY(290deg) translateX(0px) translateY(' + index * 0 + 'px);left:' + (index) * 50 + 'px;top:' + index * -20 + 'px;'">
-					<div class="side_box _top"></div>
-					<div class="side_box _left"></div>
-					<div class="side_box _right"></div>
-				</div>
-			</div>
-			<!-- <div class="rode"></div>
-			<div class="rode"></div> -->
+			<canvas ref="prismCanvas" width="500" height="500"></canvas>
 		</div>
-
-
-		<canvas id="mycanvas" class="mappanel_box_canvas" width="1000" height="700"></canvas>
 	</div>
 </template>
 
@@ -43,62 +12,67 @@ export default {
 		return {
 			pointData: [
 				{ x1: 900, y1: 100, x2: 80, y2: 75, text: '巡邏點1' },
-				{ x1: 80, y1: 75, x2: 200, y2: 300, text: '巡邏點2' },
-				{ x1: 200, y1: 300, x2: 580, y2: 500, text: '巡邏點3' },
-				{ x1: 580, y1: 500, x2: 900, y2: 100, text: '巡邏點4' },
-			]
+			],
 		}
 	},
 	created() {
-		this.doDraw();
+
 	},
 	mounted() {
-		let cw=document.querySelectorAll('#mappanel_box .mappanel_box_row:nth-child(1) > .side');
-
-		console.log('cw',cw);
-		// let mappanel_box_h=document.getElementById('mappanel_box');
-		// let canvas=document.getElementById('mycanvas');
-		// canvas.style=`width:${ mappanel_box[0].offsetWidth }px; height:${ mappanel_box_h.offsetHeight }px;`
-		// console.log('w', mappanel_box[0].offsetWidth);
-		// console.log('w', mappanel_box[0].offsetHeight);
+		this.drawPrism();
 	},
 	methods: {
-		doDraw() {
-			let canvas=document.getElementById('mycanvas');
-			let a=setInterval(() => {
-				canvas=document.getElementById('mycanvas');
-				if (!canvas) {
-					return false;
-				} else {
-					clearInterval(a);
-					let context=canvas.getContext('2d');
-					this.clearCanvas(context, canvas);
-					this.pointData.forEach((item) => {
-						const { x1, y1, x2, y2, text }=item
-
-						context.lineWidth=3;
-						context.lineCap='round';
-						context.strokeSytle='#fff';
-						context.fillStyle='rgb(0,255,255)';
-						context.beginPath();
-
-						// context.shadowColor='rgb(20,20,235)';
-						// context.shadowOffsetX=0;
-						// context.shadowOffsetY=0
-						// context.shadowBlur=6;
-
-						// context.setLineDash([5, 10]);
-
-						context.moveTo(x1, y1);
-						context.lineTo(x2, y2);
-
-						context.stroke();
-					})
-				}
-			})
+		drawLine(ctx, x1, y1, x2, y2) {
+			ctx.beginPath();
+			ctx.moveTo(x1, y1);
+			ctx.lineTo(x2, y2);
+			ctx.stroke();
 		},
-		clearCanvas(ctx, canvas) {
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
+		drawPrism() {
+			const canvas=this.$refs.prismCanvas;
+			const ctx=canvas.getContext('2d');
+			const x=200, y=200, width=100, height=20, depth=80;
+			const skew=depth/6;
+			const offset=depth/2;
+
+			// Draw front face (as a parallelogram)
+			ctx.strokeStyle='#DCDCDC';
+			ctx.fillStyle='#ECECEC';
+			ctx.beginPath();
+			ctx.moveTo(x, y);
+			ctx.lineTo(x+width, y-skew);
+			ctx.lineTo(x+width, y+height-skew);
+			ctx.lineTo(x, y+height);
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+
+			// Draw top face
+			ctx.fillStyle='#ffffff';
+			ctx.beginPath();
+			ctx.moveTo(x, y);
+			ctx.lineTo(x-offset, y-offset);
+			ctx.lineTo(x+width-offset, y-offset-skew);
+			ctx.lineTo(x+width, y-skew);
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+
+			// Draw left face
+			ctx.fillStyle='#ECECEC';
+			ctx.beginPath();
+			ctx.moveTo(x, y);
+			ctx.lineTo(x-offset, y-offset);
+			ctx.lineTo(x-offset, y+height-offset);
+			ctx.lineTo(x, y+height);
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+
+			// Draw edges
+			this.drawLine(ctx, x, y, x-offset, y-offset);
+			this.drawLine(ctx, x, y+height, x-offset, y+height-offset);
+			this.drawLine(ctx, x+width, y-skew, x+width-offset, y-offset-skew);
 		}
 	}
 };
---
+++ components/MapPanel_old.vue
@@ -0,0 +1,164 @@
+<template>
+	<div class="mappanel">
+		<div id="mappanel_box" class="mappanel_box" style="padding-top:200px;">
+			<div class="mappanel_box_row">
+				<div v-for="(index) in count" :key="index" class="side"
+					:style="'transform: rotateX(-30deg) rotateY(290deg) translateX(0px) translateY(' + index * 0 + 'px);left:' + (index) * 0 + 'px;top:' + index * -20 + 'px;'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+			</div>
+			<!-- <div class="mappanel_box_row" style="padding-left: 50px;">
+				<div v-for="(index) in count" :key="index" class="side"
+					:style="'transform: rotateX(-30deg) rotateY(290deg) translateX(0px) translateY(' + index * 0 + 'px);left:' + (index) * 0 + 'px;top:' + index * -20 + 'px;'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+			</div>
+			<div class="mappanel_box_row" style="padding-left: 100px;">
+				<div v-for="(index) in count" :key="index" class="side"
+					:style="'transform: rotateX(-30deg) rotateY(290deg) translateX(0px) translateY(' + index * 0 + 'px);left:' + (index) * 0 + 'px;top:' + index * -20 + 'px;'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+			</div>
+			<div class="mappanel_box_row" style="padding-left: 150px;">
+				<div v-for="(index) in count" :key="index" class="side"
+					:style="'transform: rotateX(-30deg) rotateY(290deg) translateX(0px) translateY(' + index * 0 + 'px);left:' + (index) * 0 + 'px;top:' + index * -20 + 'px;'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+			</div>
+			<div class="mappanel_box_row" style="padding-left: 200px;">
+				<div class="side" :style="'transform: rotateX(-30deg) rotateY(290deg); top:-20px'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+				<div class="side" :style="'transform: rotateX(-30deg) rotateY(290deg); top:-40px'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+				<div class="side" :style="'transform: rotateX(-30deg) rotateY(290deg); top:-60px'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+				<div class="side" :style="'transform: rotateX(-30deg) rotateY(290deg); top:-80px'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+			</div>
+			<div class="mappanel_box_row" style="padding-left: 250px;">
+				<div class="side" :style="'transform: rotateX(-30deg) rotateY(290deg); top:-20px'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+				<div class="side" :style="'transform: rotateX(-30deg) rotateY(290deg); top:-40px'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+				<div class="side" :style="'transform: rotateX(-30deg) rotateY(290deg); top:-60px'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+				<div class="side" :style="'transform: rotateX(-30deg) rotateY(290deg); top:-80px'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+			</div>
+			<div class="mappanel_box_row" style="padding-left: 300px;">
+				<div class="side" :style="'transform: rotateX(-30deg) rotateY(290deg); top:-20px'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+				<div class="side" :style="'transform: rotateX(-30deg) rotateY(290deg); top:-40px'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+				<div class="side" :style="'transform: rotateX(-30deg) rotateY(290deg); top:-60px'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+				<div class="side" :style="'transform: rotateX(-30deg) rotateY(290deg); top:-80px'">
+					<div class="side_box _top"></div>
+					<div class="side_box _left"></div>
+					<div class="side_box _right"></div>
+				</div>
+			</div> -->
+		</div>
+
+
+		<canvas id="mycanvas" class="mappanel_box_canvas" width="1000" height="700"></canvas>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			pointData: [
+				{ x1: 900, y1: 100, x2: 80, y2: 75, text: '巡邏點1' },
+			],
+			count: 1
+		}
+	},
+	created() {
+		this.doDraw();
+	},
+	mounted() {
+		let mappanel_box_h=document.getElementById('mappanel_box');
+		let canvas=document.getElementById('mycanvas');
+		canvas.style=`width:${ this.count*115 }px; height:${ mappanel_box_h.offsetHeight }px;`
+	},
+	methods: {
+		doDraw() {
+			let canvas=document.getElementById('mycanvas');
+			let a=setInterval(() => {
+				canvas=document.getElementById('mycanvas');
+				if (!canvas) {
+					return false;
+				} else {
+					clearInterval(a);
+					let context=canvas.getContext('2d');
+					this.clearCanvas(context, canvas);
+					this.pointData.forEach((item) => {
+						const { x1, y1, x2, y2, text }=item
+
+						context.beginPath();
+						context.lineWidth=3;
+						context.lineCap='round';
+						context.strokeStyle="white";
+
+						// context.shadowColor='#EFEFEF';
+						context.shadowColor='#EFEFEF';
+						context.shadowOffsetX=0;
+						context.shadowOffsetY=0;
+						context.shadowBlur=1;
+
+						context.moveTo(x1, y1);
+						context.lineTo(x2, y2);
+						context.stroke();
+					})
+				}
+			})
+		},
+		clearCanvas(ctx, canvas) {
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+		}
+	}
+};
+</script>
