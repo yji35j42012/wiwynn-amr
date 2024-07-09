ff47eb5fffa2eba87434992b390c113c4e266dda
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2024-07-10 00:38:59 +0800

    檔案同步

--- components/MapPanel.vue
+++ components/MapPanel.vue
@@ -1,7 +1,22 @@
+<style>
+.ball{
+	position: absolute;
+	top: 650px;
+	left: 950px;
+	width: 30px;
+	height: 30px;
+	background-color: #EE6362;
+	border-radius: 100%;
+}
+</style>
+
 <template>
 	<div class="mappanel">
 		<div id="mappanel_box" class="mappanel_box">
-			<canvas ref="prismCanvas" width="500" height="500"></canvas>
+			<canvas ref="prismCanvas" width="1800" height="1000"></canvas>
+
+
+			<div class="ball"></div>
 		</div>
 	</div>
 </template>
@@ -11,13 +26,52 @@ export default {
 	data() {
 		return {
 			pointData: [
-				{ x: 100, y: 200, w: 100, h: 20, d: 80, text: '物件1' },
-				{ x: 225, y: 182, w: 100, h: 20, d: 80, text: '物件2' },
+				// { x: 100, y: 222, w: 100, h: 20, d: 80, text: '物件1' },
+				// { x: 225, y: 204, w: 100, h: 20, d: 80, text: '物件2' },
+				// { x: 350, y: 186, w: 100, h: 20, d: 80, text: '物件3' },
+				// { x: 475, y: 168, w: 100, h: 20, d: 80, text: '物件3' },
+				// { x: 600, y: 150, w: 100, h: 20, d: 80, text: '物件3' },
+				// { x: 725, y: 132, w: 100, h: 20, d: 80, text: '物件3' },
+				// { x: 850, y: 114, w: 100, h: 20, d: 80, text: '物件3' },
+				// { x: 975, y: 96, w: 100, h: 20, d: 80, text: '物件3' },
+				// { x: 1100, y: 78, w: 100, h: 20, d: 80, text: '物件3' },
+				// { x: 1225, y: 60, w: 100, h: 20, d: 80, text: '物件3' },
+
+
+
+				// { x: 180, y: 320, w: 100, h: 20, d: 80, text: '物件1' },
+				{ x: 490, y: 710, w: 100, h: 20, d: 80, text: '物件1' },
+				{ x: 615, y: 692, w: 100, h: 20, d: 80, text: '物件1' },
+				{ x: 740, y: 674, w: 100, h: 20, d: 80, text: '物件1' },
+				{ x: 1365, y: 584, w: 100, h: 20, d: 80, text: '物件1' },
+				{ x: 1490, y: 566, w: 100, h: 20, d: 80, text: '物件1' },
+				{ x: 1615, y: 548, w: 100, h: 20, d: 80, text: '物件1' },
+
+				
+				{ x: 535, y: 760, w: 100, h: 20, d: 80, text: '物件1' },
+				{ x: 660, y: 742, w: 100, h: 20, d: 80, text: '物件1' },
+				{ x: 785, y: 724, w: 100, h: 20, d: 80, text: '物件1' },
+				{ x: 1410, y: 634, w: 100, h: 20, d: 80, text: '物件1' },
+				{ x: 1535, y: 616, w: 100, h: 20, d: 80, text: '物件1' },
+				{ x: 1660, y: 598, w: 100, h: 20, d: 80, text: '物件1' },
+
 			],
 		}
 	},
 	created() {
-
+		for(var i =0; i<10;i++){
+			this.pointData.push(
+				{x:i*125+100,y:222-18*i,w:100,h:20,d:80,text: '物件1'},
+				{x:i*125+180,y:320-18*i,w:100,h:20,d:80,text: '物件1'},
+				{x:i*125+410,y:610-18*i,w:100,h:20,d:80,text: '物件1'}
+			)
+		}
+		for(var i =0; i<9;i++){
+			this.pointData.push(
+				{x:i*125+380,y:400-18*i,w:100,h:20,d:80,text: '物件1'},
+				{x:i*125+460,y:498-18*i,w:100,h:20,d:80,text: '物件1'},
+			)
+		}
 	},
 	mounted() {		
 		this.pointData.forEach(item => {
